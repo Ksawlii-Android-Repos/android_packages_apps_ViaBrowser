c0449b843687663a48a95b6cac16d42817d81a5d
Ksawlii <ksawlii.do.plikow@gmail.com>
2024-09-21 21:40:05 +0000

    Initial

---
+++ Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := ViaBrowser
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := viabrowser-v5.9.0.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED : true
+include $(BUILD_PREBUILT)
